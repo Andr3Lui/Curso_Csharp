1bf876d12199a05d0d7b5956cc1c7d44a64f6657
André Luiz <andre.lpsilva7@senacsp.edu.br>
2025-01-16 19:27:17 -0300

    Adicionar arquivos de projeto.

---
+++ EX005/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EX005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1, num2, num3;
+
+            Console.WriteLine("Digite a primeira nota");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a primeira nota");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            num3 = (num1 + num2) / 2;
+
+            Console.WriteLine("({0} + {1}) /2 = {2}", num1 , num2, num3);
+        }
+    }
+}
