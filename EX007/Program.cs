d56d6bb01d9f1486e4538ecd8a8638326a4829c6
André Luiz <andre.lpsilva7@senacsp.edu.br>
2025-01-16 21:49:29 -0300

    adicionado "aprovado com louvor!", e mensagem de nota e status do aluno.

--- EX007/Program.cs
+++ EX007/Program.cs
@@ -4,23 +4,33 @@
     {
         static void Main(string[] args)
         {
-            double nota_final = 30;
+            double nota_final = 29;
             string resultado;
 
             if (nota_final >= 60)
             {
-                resultado = "Aprovado";
+                if (nota_final >= 90)
+                {
+                    resultado = "Aprovado com louvor!";
+                }
+                else
+                {
+                    resultado = "Aprovado";
+                }
             }
-            else if (nota_final >= 40)
+            else
             {
-                resultado = "de recuperação";
-            }
-            else 
-            {
-                resultado = "Reprovado";
+                if (nota_final >= 40)
+                {
+                    resultado = "de recuperação";
+                }
+                else
+                {
+                    resultado = "Reprovado";
+                }
             }
 
-            Console.WriteLine("O aluno está {0}.", resultado);
+            Console.WriteLine("Notado do Aluno: {0} - O aluno está {1}.",nota_final, resultado);
 
         }
     }
