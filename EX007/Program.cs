c1c08b091c16cbff2940f0f5fe49aae15d7fe0a1
André Luiz <andre.lpsilva7@senacsp.edu.br>
2025-01-16 21:23:05 -0300

    Adicionando Recuperação.

--- EX007/Program.cs
+++ EX007/Program.cs
@@ -4,19 +4,23 @@
     {
         static void Main(string[] args)
         {
-            double nota_final = 70;
+            double nota_final = 30;
             string resultado;
 
             if (nota_final >= 60)
             {
                 resultado = "Aprovado";
             }
+            else if (nota_final >= 40)
+            {
+                resultado = "de recuperação";
+            }
             else 
             {
                 resultado = "Reprovado";
             }
 
-            Console.WriteLine("O aluno foi {0}.", resultado);
+            Console.WriteLine("O aluno está {0}.", resultado);
 
         }
     }
