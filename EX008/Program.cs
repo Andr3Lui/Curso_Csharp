34c7b5b039845cbaba3f7b2a90eb33fe5a08b705
André Luiz <andre.lpsilva7@senacsp.edu.br>
2025-01-21 18:47:41 -0300

    sistema de reconhecimento incluido.

---
+++ EX008/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EX008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            string resultado;
+
+            Console.Write("Digite um número:");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                resultado = "Par";
+            }
+            else
+            {
+                resultado = "Ímpar";
+            }
+
+            Console.WriteLine("{0}", resultado);
+
+        }
+    }
+}
