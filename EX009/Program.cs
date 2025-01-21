95d015de485b07c209c061fe36acb9b34ec17c1d
André Luiz <andre.lpsilva7@senacsp.edu.br>
2025-01-21 20:12:24 -0300

    sistema de reconhecimento de tipos de triângulo incluso.

--- EX009/Program.cs
+++ EX009/Program.cs
@@ -20,11 +20,27 @@
             if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoB))
             {
                 Console.WriteLine("Os valores informados foram um triângulo:");
+
+                if ((ladoA == ladoB) && (ladoB == ladoC))
+                {
+                    Console.WriteLine("E ele é um Triângulo Equilatero.");
+                }
+                else if ((ladoA == ladoB) || (ladoA == ladoC) || (ladoB== ladoC))
+                {
+                    Console.WriteLine("Ele é um triângulo Isósiceles.");
+                }
+                else
+                {
+                    Console.WriteLine("Ele é um Triângulo Escaleno.");
+                }
+
+
             }
             else
             {
                 Console.WriteLine("Os valores informados não formam um triângulo.");
             }
+
         }
     }
 }
