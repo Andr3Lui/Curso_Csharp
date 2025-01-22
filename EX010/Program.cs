f177b3d31354bec4d9e6c9bd765716247b681381
André Luiz <andre.lpsilva7@senacsp.edu.br>
2025-01-21 22:08:22 -0300

    programa para calculo de aumento de salário.

---
+++ EX010/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace EX010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, aumento;
+
+            Console.WriteLine("");
+            Console.WriteLine("Digite aqui seu salario:");
+            Console.WriteLine("");
+
+            salario = Convert.ToDouble(Console.ReadLine());
+
+            if (salario > 2500)
+            {
+                aumento = (salario * 0.05);
+                Console.WriteLine("Seu aumento é de: {0}", aumento);
+                aumento = salario + (salario * 0.05);
+                Console.WriteLine("Seu salário final é de: {0}", aumento);
+            }
+            else if (salario > 1250)
+            {
+                aumento = (salario * 0.10);
+                Console.WriteLine("Seu aumento é de: {0}", aumento);
+                aumento = salario + (salario * 0.10);
+                Console.WriteLine("Seu salário final é de: {0}", aumento);
+            }
+            else if(salario <=1250)
+            {
+                aumento = (salario * 0.15);
+                Console.WriteLine("Seu aumento é de: {0}", aumento);
+                aumento = salario + (salario * 0.15);
+                Console.WriteLine("Seu salário final é de: {0}", aumento);
+            }
+            
+          
+            
+                
+           
+        }
+    }
+}
