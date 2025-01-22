901e547a66dc832f159c7c1211cd0d5b3c260473
André Luiz <andre.lpsilva7@senacsp.edu.br>
2025-01-21 22:28:00 -0300

    EX011.

---
+++ EX011/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EX011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===================");
+            Console.WriteLine("Emprestimo Bancário");
+            Console.WriteLine("===================");
+            double casa, salario;
+            int anos;
+
+            Console.WriteLine("Digite o valor da casa desejada:");
+            casa = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite seu salário:");
+            salario = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite em quantos deseja pagar:");
+            anos = Convert.ToInt32(Console.ReadLine());
+            
+           
+            
+        }
+    }
+}
