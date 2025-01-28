58bf6b7df40c61aa5161cd023a1737aea4508f87
André Luiz <andre.lpsilva7@senacsp.edu.br>
2025-01-28 18:55:33 -0300

    teste.

---
+++ EX012/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EX012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 3;
+
+            /*
+
+            while (num < 5)
+            {
+                num++;
+                Console.WriteLine("Curso C#");
+            }
+            */
+            
+            do
+            {
+                num++;
+                Console.WriteLine("Curso C#");
+            }while (num < 5);
+        }
+    }
+}
