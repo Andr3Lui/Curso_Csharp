ba188f9cc42cd162878e5750be8f84396f97ed67
André Luiz <andre.lpsilva7@senacsp.edu.br>
2025-01-28 21:39:06 -0300

    Diferentes Vetores.

--- Vetor/Program.cs
+++ Vetor/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace Vetor
+﻿using System;
+using System.Globalization;
+
+namespace Vetor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            /*
             int[] num = new int[5];
 
             num[0] = 10;
@@ -19,6 +24,42 @@
 
             int[] numero = { 67, 87, 90, 43, 12, 8, 4, 8, 78 };
             Console.WriteLine(numero[6]);
-        }
+            
+
+            Random random = new Random();
+            int[] vetor = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                vetor[i] = random.Next(1, 100);
+                Console.WriteLine(vetor[i]);
+            }
+
+            Console.WriteLine();
+            double[] reais = new double[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                reais[i] = random.NextDouble() * 10000;
+                Console.WriteLine(reais[i].ToString("C2"));
+            }
+            */
+
+            string[] nomes = { "Maria", "João", "Carlos", "Ana", "Pedro", "José", "Neymar", "Vini JR" };
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                if(i < nomes.Length - 1)
+                {
+                    Console.Write(nomes[i] + ", ");
+                }
+                else
+                {
+                    Console.WriteLine(nomes[i] + ".");
+                }
+                
+            }
+
+        }   
     }
 }
